1a130c5598391e65e63c6b2a7dc7340aedd62698
turaz <khanturaz9@gmail.com>
2023-09-21 13:46:51 -0400

    commit

---
+++ Assignment2/src/HW/Newhw.java
@@ -0,0 +1,56 @@
+package HW;
+
+public class Newhw {
+
+	public static void main(String[] args) {
+	/*
+		//hw 1
+		
+		String weather;
+		weather=30;
+		
+		
+		if ((weather>=80)&& (weather<=100)) {
+		}
+		else if ((weather>=60)&& (weather<=700)){
+			System.out.println("mostly cloud");
+		}
+		else if ((weather>=30)&& (weather<=50)) {
+			System.out.println("partly cloud");
+		}
+		
+		else((weather<30)&& (weather>=0)) {
+			System.out.println("sunny");
+		}
+		*/
+	/*
+		//hw 2
+		int age=20;
+		int equifax=500,transunion=650,experian=700;
+		
+		if ((age>=18) && (equifax>=600)&& (transunion>650)&&(experian>700)){
+		System.out.println("Eligible");
+		}
+		else{
+			System.out.println("NotEligible");
+			
+		}*/
+		//hw 3
+		double worked=10;
+		int age = 55;
+		double annualearning=0;
+		if ((worked>=10)&&(age>=65)&&(annualearning==0)) {
+			System.out.println("medicare eligible");
+		}
+		else {
+			System.out.println("Not Eligible");
+		}
+		
+		
+		
+		
+		
+		
+		
+	}
+}
---
+++ Assignment2/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author TURAZ
+ *
+ */
+module Assignment2 {
+}
